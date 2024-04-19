b08772038ce17dbb94081a0f65f0ee5ac1019822
Fernanda-Grabicoski <116107811+Fernanda-Grabicoski@users.noreply.github.com>
2024-04-19 10:58:56 -0300

    .

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 function atualizaCronometro(){ 
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0)[0];
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
     document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];  
     document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
     document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
